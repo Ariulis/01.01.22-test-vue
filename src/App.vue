7e69bca46c82635a8cc78c483abe0345e0fc6647
Ariulis <andrey59995@gmail.com>
2022-01-01 11:15:13 +0200

    changed App.vue, HelloWorld.vue files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="It's my second vue application"/>
   </div>
 </template>
 
